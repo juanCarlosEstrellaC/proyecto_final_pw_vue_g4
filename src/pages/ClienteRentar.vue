0ef7b58971ec4cc760ed38f2e4eaa38a2f31edcd
Juan Estrella <jcestrella@uce.edu.ec>
2024-02-25 18:07:12 -0500

    TablaRentar

--- src/pages/ClienteRentar.vue
+++ src/pages/ClienteRentar.vue
@@ -1,13 +1,135 @@
 <template>
   <h1>Rentar</h1>
+  <div class="contenedor">
+    <label for="">Marca:</label>
+    <input type="text" v-model="marca" />
+    <label for="">Modelo:</label>
+    <input type="text" v-model="modelo" />
+    <button @click="presionarBoton">Buscar</button>
+  </div>
+
+  <div class="tabla">
+    <table>
+      <thead>
+        <tr>
+          <th>Marca</th>
+          <th>Modelo</th>
+          <th>Placa</th>
+          <th>Año de Fabricación</th>
+          <th>Cilindraje</th>
+          <th>Avalúo</th>
+          <th>Precio Diario</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="vehiculo in listadoVehiculos" :key="vehiculo.id">
+          <td>{{ vehiculo.marca }}</td>
+          <td>{{ vehiculo.modelo }}</td>
+          <td>{{ vehiculo.placa }}</td>
+          <td>{{ vehiculo.anioFabricacion }}</td>
+          <td>{{ vehiculo.cilindraje }}</td>
+          <td>{{ vehiculo.avaluo }}</td>
+          <td>{{ vehiculo.precioDiario }}</td>
+          <td><button>Rentar</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      marca: null,
+      modelo: null,
+      listadoVehiculos: [
+        {
+          id: 1,
+          marca: "Toyota",
+          modelo: "Corolla",
+          placa: "ABC123",
+          anioFabricacion: 2020,
+          cilindraje: 2000,
+          avaluo: 25000,
+          precioDiario: 50,
+          rentar: true,
+        },
+        {
+          id: 2,
+          marca: "Honda",
+          modelo: "Civic",
+          placa: "XYZ789",
+          anioFabricacion: 2018,
+          cilindraje: 1800,
+          avaluo: 22000,
+          precioDiario: 45,
+          rentar: false,
+        },
+        {
+          id: 3,
+          marca: "Ford",
+          modelo: "Mustang",
+          placa: "DEF456",
+          anioFabricacion: 2019,
+          cilindraje: 2500,
+          avaluo: 35000,
+          precioDiario: 60,
+          rentar: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    presionarBoton() {
+      const vehiculo = {
+        marca: this.marca,
+        modelo: this.modelo,
+      };
+      this.$emit("miEvento", cliente);
+    },
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.contenedor {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+  width: 300px;
+  background-color: rgb(241, 222, 198);
+  border: solid 1px black;
+}
+.tabla{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 
+table {
+    width: 90%;
+    border-collapse: collapse;
+  }
+
+  th, td {
+    border: 1px solid #dddddd;
+    text-align: center;
+    padding: 8px;
+    width: calc(100% / 8);
+  }
+
+  th {
+    background-color: #4CAF50;
+    color: white;
+  }
+
+button {
+  margin: 10px;
+  background-color: rgb(255, 206, 127);
+  border: solid 1px black;
+}
 </style>
